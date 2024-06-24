72d19d05a8a3c1a99ad400cd0447a7cc6209bed3
Danil <danil_karashchuk@mail.ru>
2024-06-24 17:23:16 +0300

    Добавление js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const btn = document.querySelector('.profile__add-button')
+const list = document.querySelector('.places__list')
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+function addCard() {
+    initialCards.map((item) => {
+        const elem = document.querySelector('#card-template').content
+        let card = elem.querySelector('.card').cloneNode(true)
+        card.querySelector('.card__image').src = item.link
+        card.querySelector('.card__title').textContent = item.name
+        list.append(card)
+    })
 
-// @todo: Функция удаления карточки
+    const btnDelete = document.querySelectorAll('.card__delete-button');
+    btnDelete.forEach(elem => {
+        elem.addEventListener('click', function(event) {
+            const listItem = event.target.closest('.card')
+            listItem.remove()
+        })
+    })
 
-// @todo: Вывести карточки на страницу
+}
+
+
+
+btn.addEventListener('click', function() {
+    addCard()
+})
