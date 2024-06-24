34ee344836e5dc81fc4f4f17c02c230172ff7dc2
Danil <danil_karashchuk@mail.ru>
2024-06-24 21:12:16 +0300

    Исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
-const btn = document.querySelector('.profile__add-button')
-const list = document.querySelector('.places__list')
+
+const elem = document.querySelector('#card-template').content
+const cardsContainer = document.querySelector('.places__list')
 
 
-function addCard() {
-    initialCards.map((item) => {
-        const elem = document.querySelector('#card-template').content
-        let card = elem.querySelector('.card').cloneNode(true)
-        card.querySelector('.card__image').src = item.link
-        card.querySelector('.card__title').textContent = item.name
-        list.append(card)
-    })
+initialCards.forEach((item) => {
+    const cardElement = createCard(item,deleteCard)
+    cardsContainer.append(cardElement)
+})
 
-    const btnDelete = document.querySelectorAll('.card__delete-button');
-    btnDelete.forEach(elem => {
-        elem.addEventListener('click', function(event) {
-            const listItem = event.target.closest('.card')
+
+function deleteCard (event) {
+    const listItem = event.target.closest('.card')
             listItem.remove()
-        })
-    })
-
 }
 
+function createCard(item, deleteCard) {
+  const  card = elem.querySelector('.card').cloneNode(true)
+    card.querySelector('.card__image').src = item.link
+    card.querySelector('.card__image').alt = 'фото'
+    card.querySelector('.card__title').textContent = item.name
 
 
-btn.addEventListener('click', function() {
-    addCard()
-})
+    card.addEventListener('click', deleteCard)
+
+        return card
+}
+
